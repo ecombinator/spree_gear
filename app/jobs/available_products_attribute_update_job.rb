1f02e5f2ae466fe01011a142e6f51027b2e93e59
Ecombinator <ecombinator@protonmail.com>
2020-02-20 16:50:15 -0500

    Adds opengraph properties

--- app/jobs/available_products_attribute_update_job.rb
+++ app/jobs/available_products_attribute_update_job.rb
@@ -2,6 +2,7 @@ class AvailableProductsAttributeUpdateJob < ApplicationJob
   def perform
     Spree::Product.available.each do |product|
       ProductAttributeUpdateJob.perform_now(product)
+      sleep 0.250
     end
   end
 end
